7944a44ada3747c021eeb5d21b2100304c509c26
Chalamarla Tirumalesh <jssimide@gmail.com>
2010-12-14 11:39:51 +0530

    added some more ignore files

---
+++ src/simmake/ProtocolRepo.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class ProtocolRepo {
+
+	/**
+	 *
+	 *Define Mac protocols bellow
+	 */
+	public static final Mac_802_11	1;
+	public static final mac_dump	2;
+	public static final mac_loop	3;
+	//any other mac implementations follow
+
+	/**
+	 *
+	 *Define Network layer Protocols
+	 */
+	public static final IPV4	4;
+	public static final IPV6	5;
+	//any other network layer protocols follow
+
+	/**
+	 *
+	 *Define Transport layer Protocols
+	 */
+	 public static final TCP	6;
+	 public static final UDP	7;
+	 //any other transport layer protocols follow
+
+
+
+
+
+
+}
+
